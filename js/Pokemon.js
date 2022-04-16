5d984d921553500c276bf74517e9d81a4058a1c8
zamorskimarek <zamorski.marek@gmail.com>
2022-04-16 15:28:12 +0200

    finished task

--- js/Pokemon.js
+++ js/Pokemon.js
@@ -1,3 +1,11 @@
+//NOTE !!! In this task I was asked to get familiar with documentation of the API I would be using.
+//As this is a free API the creators were asking to locally cache resources whenever we request them.
+// In order to minimize the amount of request to their API I have decided to store the data I have already
+// fetched to localStorage. With every Pokemon we want to display app is checking for if this Pokemon already
+// exists in your localStorage. If so, the Pokemon data will be taken from localStorage instead of sending
+// another request. By using this app you agree to save data in your localStorage.
+// If you are done with using this app please consider clicking on "clear localStorage" button.
+
 import React, {useState} from "react";
 
 
