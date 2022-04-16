c8f3a71ad47420634f197d17cc1d86b43aa70a05
zamorskimarek <zamorski.marek@gmail.com>
2022-04-16 11:38:03 +0200

    changes for pokemons

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,181 +1,25 @@
-// import React, {useEffect, useState} from "react";
-// import {Pokemon} from "./pokemon";
-//
-// const Pokedex = () => {
-//     const [pokemonList, setPokemnList] = useState([]);
-//     const [next, setNext] = useState(20)
-//
-//     useEffect(() => {
-//         for (let i = next - 19; i <= next; i++) {
-//             if (localStorage.getItem(i) === null) {
-//                 fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-//                     .then(response => response.json())
-//                     .then(data => {
-//                         // console.log(data);
-//                         // setPokemnList(prevState => [...prevState, data])
-//                         let pok = {
-//                             name: data.name,
-//                             id: data.id,
-//                             weight: data.weight,
-//                             height: data.height,
-//                             types: data.types,
-//                             spriteFront: data.sprites.front_default,
-//                             spriteBack: data.sprites.back_default,
-//                         }
-//                         localStorage.setItem(data.id, JSON.stringify(pok))
-//                         setPokemnList(prevState => [...prevState, pok])
-//                     })
-//                     .catch(error => console.log("ERROR", error))
-//             } else {
-//                 setPokemnList(prevState => [...prevState, JSON.parse(localStorage.getItem(i))])
-//             }
-//         }
-//     }, [next])
-//
-//     const handleMore = () => {
-//         setNext(prevState => prevState + 20)
-//     }
-//
-//     // console.log(pokemonList)
-//
-//     const handleDarkMode = () => {
-//         let element = document.body;
-//         element.classList.toggle("dark-mode");
-//     }
-//
-//
-//     const [searchedPokemon, setSearchedPokemon] = useState(null)
-//     const [search, setSearch] = useState("")
-//
-//     const handleSearch = (e) => {
-//         e.preventDefault()
-//         if (isNaN(search)) {
-//             let id = search.toLowerCase()
-//             fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-//                 .then(r => r.json())
-//                 .then(data => {
-//                     let pok = {
-//                         name: data.name,
-//                         id: data.id,
-//                         weight: data.weight,
-//                         height: data.height,
-//                         types: data.types,
-//                         spriteFront: data.sprites.front_default,
-//                         spriteBack: data.sprites.back_default,
-//                     }
-//                     setSearchedPokemon(pok)
-//                 })
-//                 .catch(err => {
-//                     console.warn(err)
-//                     alert("There is no Pokemon with this name in database. Please check your spelling and try again.")
-//                 })
-//         } else {
-//             localStorage.getItem(search) ?
-//                 (setSearchedPokemon(JSON.parse(localStorage.getItem(search)))) :
-//                 (fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
-//                     .then(r => r.json())
-//                     .then(data => {
-//                         let pok = {
-//                             name: data.name,
-//                             id: data.id,
-//                             weight: data.weight,
-//                             height: data.height,
-//                             types: data.types,
-//                             spriteFront: data.sprites.front_default,
-//                             spriteBack: data.sprites.back_default,
-//                         }
-//                         setSearchedPokemon(pok)
-//                     })
-//                     .catch(err => {
-//                         console.warn(err)
-//                         alert("There is no Pokemon with this ID in database. Please try again.")
-//                     }))
-//         }
-//         setSearch("")
-//     }
-//
-//      const handleCloseSearched = () => {
-//         setSearchedPokemon(null)
-//     }
-//
-//     const handleClear = () => {
-//         localStorage.clear()
-//     }
-//
-//     pokemonList.sort(function(a, b) {
-//         return a.id - b.id  ||  a.name.localeCompare(b.name);
-//     });
-//
-//
-//
-//     return !pokemonList.length ? (
-//         <div className="loader-position">
-//             <div className="loader"></div>
-//         </div>
-//     ) : (
-//         <div className="container">
-//             <h1 className="center-text">PokeDex App</h1>
-//             <div className="menu">
-//                 <button onClick={handleDarkMode} className="btn">Toggle dark mode</button>
-//                 {/*<button className="button" onClick={handleMore}>show me more</button>*/}
-//                 <form onSubmit={handleSearch}>
-//                     <input type="text" placeholder="search by ID or name" className="input-text" value={search} onChange={e => setSearch(e.target.value)}/>
-//                     <button type="submit" className="btn">Search</button>
-//                 </form>
-//             </div>
-//             <div className="search-results">
-//                 {searchedPokemon !== null && <p>Here is a Pokemon you were searching for !!!</p>}
-//                 {searchedPokemon !== null && <Pokemon{...searchedPokemon}></Pokemon>}
-//                 {searchedPokemon !== null && <button className="btn" onClick={handleCloseSearched}>CLOSE</button>}
-//             </div>
-//
-//             {/*<button onClick={handleMore}>show me more</button>*/}
-//             <ul className="listaPoke">
-//                 {
-//                     pokemonList.map(pokemon => (
-//                         <Pokemon key={pokemon.id} {...pokemon}></Pokemon>
-//                         )
-//                     )
-//                 }
-//             </ul>
-//             <div className="center-hor">
-//                 <button className="button btn" onClick={handleMore}>show me more</button>
-//                 <button className="button btn" onClick={handleClear}>clear localStorage</button>
-//             </div>
-//         </div>
-//     )
-// }
-//
-// export default Pokedex
-
-
-
-
 import React, {useEffect, useState} from "react";
 import {Pokemon} from "./pokemon";
+
 
 const Pokedex = () => {
     const [pokemonList, setPokemnList] = useState([]);
     const [next, setNext] = useState(20)
     const [localLoaded, setLocaleLoaded] = useState(false)
-
+    const [count, setCount] = useState(null)
 
     const useURLs = () => {
         fetch(" https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
             .then(response => response.json())
             .then(data => {
-                let count = data.count
+                setCount(data.count)
                 let rawData = data.results
                 let arrayOfIds = []
-                console.log(data.results)
-                console.log(rawData)
                 rawData.map(el => arrayOfIds.push(el.url.replace("https://pokeapi.co/api/v2/pokemon/","")))
-                console.log(arrayOfIds)
                 localStorage.setItem("arrayOfIds", JSON.stringify(arrayOfIds))
-                console.log(count)
                 setLocaleLoaded(true)
             })
-            .catch(err => console.log(err))
+            .catch(err => alert(err))
     }
 
 
@@ -215,8 +59,6 @@ const Pokedex = () => {
     const handleMore = () => {
         setNext(prevState => prevState + 20)
     }
-
-    // console.log(pokemonList)
 
     const handleDarkMode = () => {
         let element = document.body;
@@ -282,11 +124,15 @@ const Pokedex = () => {
 
     const handleClear = () => {
         localStorage.clear()
+        alert("Your Local Storage has been cleared")
+        setPokemnList([])
+        setLocaleLoaded(false)
+        setNext(20)
     }
 
     pokemonList.sort(function(a, b) {
-        return a.id - b.id  ||  a.name.localeCompare(b.name);
-    });
+        return a.id - b.id
+    })
 
 
 
@@ -299,18 +145,17 @@ const Pokedex = () => {
             <h1 className="center-text">PokeDex App</h1>
             <div className="menu">
                 <button onClick={handleDarkMode} className="btn">Toggle dark mode</button>
-                {/*<button className="button" onClick={handleMore}>show me more</button>*/}
                 <form onSubmit={handleSearch}>
                     <input type="text" placeholder="search by ID or name" className="input-text" value={search} onChange={e => setSearch(e.target.value)}/>
                     <button type="submit" className="btn">Search</button>
                 </form>
             </div>
             <div className="search-results">
-                {searchedPokemon !== null && <p>Here is a Pokemon you were searching for !!!</p>}
+                {searchedPokemon !== null && <p className="bold">Here is a Pokemon you were searching for !!!</p>}
                 {searchedPokemon !== null && <Pokemon{...searchedPokemon}></Pokemon>}
                 {searchedPokemon !== null && <button className="btn" onClick={handleCloseSearched}>CLOSE</button>}
             </div>
-            <ul className="listaPoke">
+            <ul className="poke-list">
                 {
                     pokemonList.map(pokemon => (
                             <Pokemon key={pokemon.id} {...pokemon}></Pokemon>
@@ -319,8 +164,8 @@ const Pokedex = () => {
                 }
             </ul>
             <div className="center-hor">
-                <button className="button btn" onClick={handleMore}>show me more</button>
-                <button className="button btn" onClick={handleClear}>clear localStorage</button>
+                {(pokemonList.length !== count) && <button className="btn-big btn" onClick={handleMore}>show me more</button>}
+                <button className="btn-big btn" onClick={handleClear}>clear localStorage</button>
             </div>
         </div>
     )
--- js/pokemon.js
+++ js/pokemon.js
@@ -1,41 +1,34 @@
 import React, {useState} from "react";
 
+
 export const Pokemon = ({...pokemon}) => {
     const [pokemonLoading, setPokemonLoading] = useState(true)
 
-        return (
-            <li className="pokemon-card">
-                <div className="pokemon-card__content">
-                    <div className="pokemon-card__front">
-                        <p className="pokemon-card__front__content capitalize qqq">{pokemon.name}</p>
-                        <div className="pokemon-card__front__content zzz">Poke ID: {pokemon.id}</div>
-                        <div className="types-display pokemon-card__front__content">
-                            {pokemon.types.length === 1 ? (<span>Type:{"\u00a0"}</span>) : (<span>Types:{"\u00a0"}</span>)}
-                            {pokemon.types.map((el, index) => <span key={index}>{el.type.name}{"\u00a0"}</span>)}
-                        </div>
-                        {pokemonLoading && (<div className="pokemon-loader-position">
-                            <div className="pokemon-loader"></div>
-                        </div>)}
-                        <img className="sprite" src={pokemon.spriteFront} onLoad={() => setPokemonLoading(false)} style={{display: "inline-block"}}></img>
+    return (
+        <li className="pokemon-card">
+            <div className="pokemon-card__content">
+                <div className="pokemon-card__front">
+                    <p className="pokemon-card__front__content capitalize bold">{pokemon.name}</p>
+                    <div className="pokemon-card__front__content poke-id">Poke ID: {pokemon.id}</div>
+                    <div className="types-display pokemon-card__front__content">
+                        {pokemon.types.length === 1 ? (<span>Type:{"\u00a0"}</span>) : (<span>Types:{"\u00a0"}</span>)}
+                        {pokemon.types.map((el, index) => <span key={index}>{el.type.name}{"\u00a0"}</span>)}
                     </div>
-                    <div className="pokemon-card__back">
-                        <div className="xxx">
-                            <p className="pokemon-card__back__content qqq">Additional Info:</p>
-                            <p className="pokemon-card__back__content">Weight: {pokemon.weight}</p>
-                            <p className="pokemon-card__back__content">Height: {pokemon.height}</p>
-                        </div>
-                        <img className="sprite" src={pokemon.spriteBack} style={{display: "inline-block"}}></img>
-
-                    </div>
+                    {pokemonLoading && (<div className="pokemon-loader-position">
+                        <div className="pokemon-loader"></div>
+                    </div>)}
+                    <img className="sprite" src={pokemon.spriteFront} onLoad={() => setPokemonLoading(false)} style={{display: "inline-block"}}></img>
                 </div>
-            </li>
-        )
-        // return (
-        //     <li className="pokemon-card">
-        //         <div className="loader-position">
-        //             <div className="loader"></div>
-        //         </div>
-        //     </li>
-        // )
+                <div className="pokemon-card__back">
+                    <div className="pokemon-info">
+                        <p className="pokemon-card__back__content bold">Additional Info:</p>
+                        <p className="pokemon-card__back__content">Weight: {pokemon.weight}</p>
+                        <p className="pokemon-card__back__content">Height: {pokemon.height}</p>
+                    </div>
+                    <img className="sprite" src={pokemon.spriteBack} style={{display: "inline-block"}}></img>
 
+                </div>
+            </div>
+        </li>
+    )
 }
